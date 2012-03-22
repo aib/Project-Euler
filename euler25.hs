64a1b9d5102a94c6ed4ab9067e65ff463eeefbcd
aib <aibok42@gmail.com>
2012-03-22 22:21:13 +0200

    #25 now uses Common.fibI

--- euler25.hs
+++ euler25.hs
@@ -1,6 +1,5 @@
-import Data.List
 import Data.Digits
 
-fib = unfoldr (\(n,x,y) -> Just ((n,x), (n+1,y,x+y))) (0, 0, 1)
+import Common
 
-main = print $ head $ dropWhile (\(n,x) -> (length $ digits 10 x) < 1000) fib
+main = print $ head $ dropWhile (\(n,x) -> (length $ digits 10 x) < 1000) fibI
