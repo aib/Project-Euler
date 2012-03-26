fd7d4e66dcdf3dce0047029bab1d93f6b0c32729
aib <aibok42@gmail.com>
2012-03-26 21:59:04 +0100

    Added palindrome function to check whether a list is a palindrome

--- Common.hs
+++ Common.hs
@@ -1,5 +1,5 @@
 module Common
-    (fib, fibI)
+    (fib, fibI, palindrome)
   where
 
 import Data.List
@@ -9,3 +9,9 @@ fib = unfoldr (\(x,y) -> Just (x, (y,x+y))) (0, 1)
 
 -- Fibonacci list, paired with indices
 fibI = unfoldr (\(n,x,y) -> Just ((n,x), (n+1,y,x+y))) (0, 0, 1)
+
+-- Palindrome predicate for lists
+palindrome :: Eq a => [a] -> Bool
+palindrome [] = True
+palindrome (x : []) = True
+palindrome (x : xs) = (x == last xs) && palindrome (take ((length xs) - 1) xs)
