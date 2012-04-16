344f19e03b06d5854c86e263ca069df65cf7a5b6
aib <aibok42@gmail.com>
2012-04-16 19:59:39 +0300

    #21

---
+++ euler21.hs
@@ -0,0 +1,14 @@
+import Math.NumberTheory.Primes.Factorisation
+import Data.Set as S
+import Data.Map as M
+
+properDivSum x = sum . S.toList . (S.delete x) . divisors $ x
+
+pdsMap = M.fromList [(n,properDivSum n) | n <- [1..9999]]
+
+amicable n = (pds /= return n) && (pds >>= ((flip M.lookup) pdsMap)) == return n
+  where
+    pds = M.lookup n pdsMap
+
+main = print $
+    sum $ Prelude.map fst $ M.toList (M.filterWithKey (const . amicable) pdsMap)
