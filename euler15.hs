0284a463cec8efbf808ba1a63f51f3bb55d41d30
aib <aibok42@gmail.com>
2012-04-16 18:12:29 +0300

    Crude graph implementation of #15

---
+++ euler15.hs
@@ -0,0 +1,31 @@
+import Data.Graph.Inductive.Graph
+import Data.Graph.Inductive.Tree
+import Data.Graph.Inductive.Graphviz
+
+width = 5
+height = 5
+
+type NodeLabel = (Int,Int,Int)
+type EdgeLabel = String
+
+makeNode :: Int -> Int -> LNode NodeLabel
+makeNode x y = (nodeId, (nodeId,x,y))
+  where
+    nodeId = y*height + x
+
+graph :: Gr NodeLabel EdgeLabel
+graph = mkGraph [makeNode x y | x <- [0..width-1], y <- [0..height-1]] graphEdges
+
+graphEdges :: [(Int,Int,EdgeLabel)]
+graphEdges = [edge (x,y) (x+1,y) | x <- [0..width-2], y <- [0..height-1]] ++
+             [edge (x,y) (x,y+1) | x <- [0..width-1], y <- [0..height-2]]
+  where
+    edge (x,y) (x',y') = (fst $ makeNode x y, fst $ makeNode x' y', show (fst $ makeNode x y) ++ "->" ++ show (fst $ makeNode x' y'))
+
+paths :: Graph g => Decomp g NodeLabel EdgeLabel -> [EdgeLabel] -> Node -> Node -> [[EdgeLabel]]
+paths d p s e | s == e = [p]
+paths (Just ct, g) p s e = (concatMap (\(ne,nn) -> paths (match nn g) (p ++ [ne]) nn e) (toAdjs ct))
+  where
+    toAdjs = (\(_,_,_,a) -> a)
+
+main = print $ length $ paths (Just (context graph 0), graph) [[]] 0 (width*height - 1)
