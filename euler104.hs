8ad5a2544fd699402bb328ad3218e8e2c4942d87
aib <aibok42@gmail.com>
2017-06-08 20:46:46 +0300

    Use Common.pandigital in #104

--- euler104.hs
+++ euler104.hs
@@ -1,9 +1,6 @@
 import Data.Digits
-import Data.Set as Set
 
 import Common
-
-pandigital = (== 9) . length . toList . (Set.filter (/= 0)) . fromList 
 
 pandigitalFirst = pandigital . (take 9) . (digits 10)
 pandigitalLast = pandigital . (take 9) . reverse . (digits 10)
