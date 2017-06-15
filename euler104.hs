72f0204df5dff8147784410a2f278f98aa3006ae
aib <aibok42@gmail.com>
2017-06-15 14:57:28 +0300

    Fix #104 to use the new, parametric pandigital

--- euler104.hs
+++ euler104.hs
@@ -2,8 +2,8 @@ import Data.Digits
 
 import Common
 
-pandigitalFirst = pandigital . (take 9) . (digits 10)
-pandigitalLast = pandigital . (take 9) . reverse . (digits 10)
+pandigitalFirst = (pandigital 9) . (take 9) . (digits 10)
+pandigitalLast = (pandigital 9) . (take 9) . reverse . (digits 10)
 
 main =
     print $ head $ Prelude.filter (\(x,y) -> pandigitalFirst y && pandigitalLast y) fibI
