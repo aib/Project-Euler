9b4c8e20942d47a1c02fa9ae82937518a3848654
aib <aibok42@gmail.com>
2017-06-15 16:06:20 +0300

    Add main function to #19

--- euler19.hs
+++ euler19.hs
@@ -1,4 +1,5 @@
 import Data.Time.Calendar
 import Data.Time.Calendar.WeekDate
 
-length $ filter (\(y,w,dayOfWeek) -> dayOfWeek == 7) [toWeekDate $ fromGregorian year mon 1 | mon <- [1..12], year <- [1901..2000]]
+main = print $
+    length $ filter (\(y,w,dayOfWeek) -> dayOfWeek == 7) [toWeekDate $ fromGregorian year mon 1 | mon <- [1..12], year <- [1901..2000]]
