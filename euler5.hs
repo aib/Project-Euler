843fd7760f3bd6ec7ecaf84c8452996368bef4ea
aib <aibok42@gmail.com>
2017-06-15 16:06:44 +0300

    Add main function to #5

--- euler5.hs
+++ euler5.hs
@@ -1,3 +1,4 @@
 import Control.Applicative
 
-head $ dropWhile (\x -> not $ all (==0) $ map (flip mod) [1..20] <*> [x]) [1..]
+main = print $
+    head $ dropWhile (\x -> not $ all (==0) $ map (flip mod) [1..20] <*> [x]) [1..]
