a01a142d360f3a30fcea69ab29d4d198160ee921
aib <aibok42@gmail.com>
2020-03-23 12:27:11 +0300

    Add alternative remained mapping to #5

--- euler5.hs
+++ euler5.hs
@@ -2,3 +2,4 @@ import Control.Applicative
 
 main = print $
     head $ dropWhile (\x -> not $ all (==0) $ map (flip mod) [1..20] <*> [x]) [1..]
+--  head $ dropWhile (\x -> not $ all (==0) $ zipWith mod (repeat x) [1..20]) [1..]
